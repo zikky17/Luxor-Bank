168ecf09aaff8b1e4fd8d08222abbf8b5eac4c40
Gustav Ivering <gustav.ivering@live.se>
2024-04-02 17:32:21 +0200

    Search and pagination is now in place. Need to check the pagination on max page

--- ServiceLibrary/Interfaces/ICustomerService.cs
+++ ServiceLibrary/Interfaces/ICustomerService.cs
@@ -12,5 +12,7 @@ namespace ServiceLibrary.Interfaces
         List<CustomerViewModel> GetAllCustomers(string sortColumn, string sortOrder, int pageNumber, string q);
 
         (List<CustomerViewModel>, List<AccountViewModel>, decimal) GetCustomerDetails(int customerId);
+
+        int GetTotalCustomersCount();
     }
 }
