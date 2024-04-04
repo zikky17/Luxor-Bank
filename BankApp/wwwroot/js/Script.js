e7a47f8f876a140c9ae4f5f80808b283f12f4417
Gustav Ivering <gustav.ivering@live.se>
2024-04-04 21:56:31 +0200

    Fixed UI for withdraw and deposit

--- BankApp/wwwroot/js/Script.js
+++ BankApp/wwwroot/js/Script.js
@@ -1,17 +1,1 @@
 ﻿
-    var modal = document.getElementById("depositSuccessModal");
-
-    var span = document.getElementsByClassName("close")[0];
-
-    if (depositResult === false) {
-        modal.style.display = "block";
-}
-
-    span.onclick = function() {
-        modal.style.display = "none";
-}
-    window.onclick = function(event) {
-  if (event.target == modal) {
-        modal.style.display = "none";
-  }
-}
