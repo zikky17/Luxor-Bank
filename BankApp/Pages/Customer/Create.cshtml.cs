3c5c9b9a54884da8f2d16ba985e98402d4853c2d
Gustav Ivering <gustav.ivering@live.se>
2024-04-05 11:51:37 +0200

    Create customer is now working! It also creates an account

--- BankApp/Pages/Customer/Create.cshtml.cs
+++ BankApp/Pages/Customer/Create.cshtml.cs
@@ -1,12 +1,77 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceLibrary.Interfaces;
+using ServiceLibrary.Models;
+using ServiceLibrary.Services;
 
 namespace BankApp.Pages.Customer
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+
+        public CreateModel(ICustomerService service)
+        {
+            _customerService = service;
+        }
+
+        private readonly ICustomerService _customerService;
+
+        public string Gender { get; set; } = null!;
+
+        public string Givenname { get; set; } = null!;
+
+        public string Surname { get; set; } = null!;
+
+        public string Streetaddress { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public string Zipcode { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public string CountryCode { get; set; } = null!;
+
+        public DateOnly? Birthday { get; set; }
+
+        public string? NationalId { get; set; }
+
+        public string? Telephonecountrycode { get; set; }
+
+        public string? Telephonenumber { get; set; }
+
+        public string? Emailaddress { get; set; }
+
         public void OnGet()
         {
+          
         }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var customer = new ServiceLibrary.Models.Customer
+                {
+                    Givenname = Givenname,
+                    Surname = Surname,
+                    Gender = Gender,
+                    Streetaddress = Streetaddress,
+                    City = City,
+                    Zipcode = Zipcode,
+                    Country = Country,
+                    CountryCode = CountryCode,
+                    Birthday = Birthday,
+                    NationalId = NationalId,
+                    Telephonecountrycode = Telephonecountrycode,
+                    Telephonenumber = Telephonenumber,
+                    Emailaddress = Emailaddress,
+                };
+
+                _customerService.CreateCustomer(customer);
+            }
+        }
+
     }
 }
--- ServiceLibrary/Interfaces/ICustomerService.cs
+++ ServiceLibrary/Interfaces/ICustomerService.cs
@@ -1,4 +1,5 @@
 ﻿using BankApp.ViewModels;
+using ServiceLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,8 @@ namespace ServiceLibrary.Interfaces
         List<CustomerViewModel> GetAllCustomers(string sortColumn, string sortOrder, int pageSize, int pageNumber, string q, out int totalCustomersCount);
 
         (List<CustomerViewModel>, List<AccountViewModel>, decimal) GetCustomerDetails(int customerId);
+
+        public void CreateCustomer(Customer customer);
 
     }
 }
