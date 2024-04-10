7ee531a788b436f5b2bdaae38359c43a6c6ab796
Gustav Ivering <gustav.ivering@live.se>
2024-04-10 06:56:22 +0200

    added more icons and ui improvements

---
+++ BankApp/Pages/User/CreateUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankApp.Pages.User
+{
+    public class CreateUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
