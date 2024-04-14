b624d0e343b061077baabc1caa0c91f8ce60eb16
Gustav Ivering <gustav.ivering@live.se>
2024-04-14 11:40:50 +0200

    Edit and Index page for users started

---
+++ BankApp/Pages/User/EditUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankApp.Pages.User
+{
+    public class EditUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
