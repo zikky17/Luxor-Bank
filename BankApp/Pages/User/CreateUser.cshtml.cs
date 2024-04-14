1ed2659265b5c93087da2840946fdb5000a487c3
Gustav Ivering <gustav.ivering@live.se>
2024-04-14 11:32:26 +0200

    crud for user started

--- BankApp/Pages/User/CreateUser.cshtml.cs
+++ BankApp/Pages/User/CreateUser.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceLibrary.ViewModels;
 
 namespace BankApp.Pages.User
 {
     public class CreateUserModel : PageModel
     {
+
+        public List<UserViewModel> Users { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool ConfirmPassword { get; set; } = true;
+
+        public string[] Role { get; set; }
+
         public void OnGet()
         {
+            
         }
     }
 }
---
+++ ServiceLibrary/ViewModels/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary.ViewModels
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool ConfirmPassword { get; set; }
+
+        public string[] Role { get; set; }
+    }
+}
